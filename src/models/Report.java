c78c1a557c53cde7fd4e5292503540e1bae73e5f
kota.yamashiro <kota19890122@gmail.com>
2020-12-23 16:55:01 +0900

    Add LoginFilter

---
+++ src/models/Report.java
@@ -0,0 +1,18 @@
+//package models;
+//
+//import javax.persistence.NamedQuery;
+//import javax.persistence.Table;
+//
+//import org.hibernate.annotations.NamedQueries;
+//
+//@Table(name = "reports")
+//@NamedQueries({
+//    @NamedQuery(
+//            name = "getAllReports",
+//            query = "SELECT r FROM Report AS r ORDER BY e"
+//            )
+//})
+//
+//public class Report {
+//
+//}
